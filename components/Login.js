133fce05394ba98895498a28ef671d2dd0c9db4a
Demehin Ibukunoluwa George <ibk2k7@gmail.com>
2021-08-04 01:06:14 +0100

    Corrected the Head content in the login page

--- components/Login.js
+++ components/Login.js
@@ -1,3 +1,4 @@
+import Head from 'next/head'
 import { signIn, signOut, useSession } from 'next-auth/client'
 import Button from "@material-tailwind/react/Button"
 import Icon from "@material-tailwind/react/Icon"
@@ -5,22 +6,28 @@ import Image from 'next/image'
 
 const Login = () => {
     return (
-        <div className="flex flex-col items-center justify-center min-h-screen py-2">
-            <Image 
-                // src="/Google_Docs.svg"
-                src="/google2.png"
-                height="300"
-                width="550"
-                objectFit="contain"
-            />
-            <Button
-                className="w-44 mt-10"
-                color="blue"
-                buttonType="filled"
-                ripple='light'
-                onClick={() => signIn()}
-            >Log in</Button>
-        </div>
+        <>
+            <Head>
+                <title>Google Docs Demo Login</title>
+                <link rel="icon" href="/Google_Docs_Logo.png" />
+            </Head>
+            <div className="flex flex-col items-center justify-center min-h-screen py-2">
+                <Image 
+                    // src="/Google_Docs.svg"
+                    src="/google2.png"
+                    height="300"
+                    width="550"
+                    objectFit="contain"
+                />
+                <Button
+                    className="w-44 mt-10"
+                    color="blue"
+                    buttonType="filled"
+                    ripple='light'
+                    onClick={() => signIn()}
+                >Log in</Button>
+            </div>
+        </>
     )
 }
 
